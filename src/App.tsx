4734a2f70d2ea0cff49c939ce3c6b025669d293f
akib8677 <akibjawed.dev@gmail.com>
2024-07-19 09:35:15 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Hello, Tailwind CSS with TypeScript!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
